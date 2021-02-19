9f0564ed7772b738b2fac71c1eef78896904241c
Budaházi Bence <budahazi.bence@petrik.hu>
2021-02-19 12:03:49 +0100

    Add project files.

---
+++ Galleria/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Galleria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flPanel
+            // 
+            this.flPanel.AutoScroll = true;
+            this.flPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flPanel.Location = new System.Drawing.Point(0, 0);
+            this.flPanel.Name = "flPanel";
+            this.flPanel.Size = new System.Drawing.Size(800, 150);
+            this.flPanel.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 150);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 411);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.flPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flPanel;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Galleria/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galleria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] fajlok = Directory.GetFiles("Kepek");
+            foreach (var fajl in fajlok)
+            {
+                Image kep = Image.FromFile(fajl);
+                PictureBox pb = new PictureBox();
+                pb.Image = kep;
+                pb.SizeMode = PictureBoxSizeMode.Zoom;
+                pb.Height = 100;
+                pb.Width = (int)((double)kep.Width / kep.Height * pb.Height);
+                //Eseménykezelőre feliratkozás.
+                pb.Click += Pb_Click;
+                /*
+                 * lambdás megoldás:
+                 */
+                /*
+               pb.Click += (send, args) =>
+               {
+                    pictureBox.Image = pb.Image; 
+               };
+               */
+                flPanel.Controls.Add(pb);
+            }
+        }
+
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            pictureBox.Image = pb.Image; 
+        }
+    }
+}
